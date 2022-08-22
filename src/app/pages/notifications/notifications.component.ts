24e9b5c664a6c2e5f8e693a410625316444bbba2
petterdlc <petterdelacruz99@outlook.com>
2022-08-22 03:09:31 -0500

    cambios nuevos

--- src/app/pages/notifications/notifications.component.ts
+++ src/app/pages/notifications/notifications.component.ts
@@ -87,6 +87,25 @@ export class NotificationsComponent{
 
   ngOnInit(){
 
+    var acc = document.getElementsByClassName("accordion");
+    var j;
+
+    for (j = 0; j < acc.length; j++) {
+      acc[j].addEventListener("click", function() {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+      });
+    } 
+
         
 
 
